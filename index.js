509c948bdbc3ceec73a4416306df7ca640afa227
Aiman Azmie <aimanaaw@gmail.com>
2019-09-26 21:16:48 -0400

    Committing the spotter exercises

--- index.js
+++ index.js
@@ -7,9 +7,9 @@ nextISSTimesForMyLocation((error, passTimes) => {
   }
   // success, print out the deets!
   const timesArray = passTimes.response;
-  for (let i =0; i < timesArray.length; i++) {
+  for (let i = 0; i < timesArray.length; i++) {
     let date = new Date(1000 * timesArray[i].risetime);
-    let length = timesArray[i].duration
+    let length = timesArray[i].duration;
     console.log(`Next pass at ${date} for ${length} seconds!`);
 
   }
--- iss.js
+++ iss.js
@@ -85,7 +85,7 @@ const nextISSTimesForMyLocation = (someCallBack) => {
     });
 
   });
-}
+};
 
 
 
